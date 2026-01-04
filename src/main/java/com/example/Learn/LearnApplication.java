bd55b9f24358e34900f61fc6f9d121093c727385
Venkata Ramana S <svenkataramana860@gmail.com>
2026-01-04 15:43:36 +0530

    Lombok added

--- src/main/java/com/example/Learn/LearnApplication.java
+++ src/main/java/com/example/Learn/LearnApplication.java
@@ -2,12 +2,14 @@ package com.example.Learn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.Learn.entities.User;
 
 @SpringBootApplication
 public class LearnApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnApplication.class, args);
+		var user = new User();
 	}
 
 }
